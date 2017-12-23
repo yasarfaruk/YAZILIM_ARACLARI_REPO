2f2986abbf693d21946aea1642483f5734389e4c
yasarfaruk <yasarfaruk-37-fb@hotmail.com>
2017-12-23 14:32:58 +0300

    yfy7

--- MatematikUygulamasi/src/main/java/Hesaplama.java
+++ MatematikUygulamasi/src/main/java/Hesaplama.java
@@ -1,20 +1,34 @@
-import java.util.Scanner;
-
 public class Hesaplama {
 
+	public static void main(String[] args){
+		islemler(19,25);
+	}
+
+	public static int islemler(int sayi1, int sayi2) {
+		int sonuc1 = topla(sayi1, sayi2);
+		int sonuc2 = carpma(sayi1, sayi2);
+		int sonuc = bolme(sonuc2, sonuc1);
+		System.out.println(sonuc);
+		return sonuc;
+	}
 	
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int say1,say2;
-        System.out.println("1. Sayýyý Giriniz: ");
-        say1 = input.nextInt();
-        System.out.println("2. Sayýyý Giriniz: ");
-        say2 = input.nextInt();
-        int sonuc=say1*say2;
-        System.out.println(say1 + "*" + say2 + "=" + sonuc);
-        
-        
-    }
+	public static int /* (4) */ topla( /* (1) */ int sayi1, /* (2) */ int sayi2 /* (3) */ ) 
+			throws UnsupportedOperationException // (5) Firlatilan hata */
+	{
+		if(sayi1>=10000000 || sayi2>=10000000) {
+			throw new UnsupportedOperationException();
+		}
+		
+		return sayi1 + sayi2;
+	}
+	
+	public static int carpma(int sayi1, int sayi2) {
+		return sayi1 * sayi2;
+	}
+	
+	public static int bolme(int sayi1, int sayi2) {
+		return sayi1 / sayi2;
+	}
 }
 
 	
--- MatematikUygulamasi/src/test/java/HesaplamaTest.java
+++ MatematikUygulamasi/src/test/java/HesaplamaTest.java
@@ -1,19 +1,78 @@
 
-import java.util.Scanner;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class HesaplamaTest {
 
+	@Test
+	public void testClass() {
+		boolean hataOlustu = false;
+		try {
+			Hesaplama hesaplama = new Hesaplama();
+		} catch(Exception e) {
+			hataOlustu = true;
+		}
+		
+		assertFalse(hataOlustu);
+	}
 	
-	public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int say1,say2;
-        System.out.println("1. Sayýyý Giriniz: ");
-        say1 = input.nextInt();
-        System.out.println("2. Sayýyýs Giriniz: ");
-        say2 = input.nextInt();
-        int sonuc=say1*say2;
-        System.out.println(say1 + "*" + say2 + "=" + sonuc);
-        
-        
-    }
+	@Test(expected = UnsupportedOperationException.class)
+	public void testToplaUnupported1() {
+		Hesaplama.topla(10000000, 3);
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void testToplaUnupported2() {
+		Hesaplama.topla(3, 10000000);
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void testToplaUnupported3() {
+		Hesaplama.topla(10000000, 10000000);
+	}
+	
+	@Test
+	public void testToplaHepsi() {
+		
+		for (int sayi1=-10000; sayi1<10001; sayi1++) {
+			for (int sayi2=-10000;sayi2<10001; sayi2++) {
+				int gercekSonuc = Hesaplama.topla(sayi1, sayi2);
+				assertEquals(sayi1+sayi2, gercekSonuc);
+			}
+		}
+		
+	}
+	
+	@Test
+	public void testIslemler() {
+		int gercekSonuc = Hesaplama.islemler(19,25);
+		assertEquals(10, gercekSonuc);
+	}
+
+	
+	@Test
+	public void testTopla25ve14() {
+		int gercekSonuc = Hesaplama.topla(25, 14);
+		assertEquals(39, gercekSonuc);
+	}
+	
+	@Test
+	public void testToplaNegatif() {
+		int gercekSonuc = Hesaplama.topla(-25, -14);
+		assertEquals(-39, gercekSonuc);
+	}
+	
+	@Test
+	public void testCarp6ve7() {
+		int gercekSonuc = Hesaplama.carpma(6, 7);
+		assertEquals(42, gercekSonuc);
+	}
+	
+	@Test
+	public void test60Bol10() {
+		int gercekSonuc = Hesaplama.bolme(60, 10);
+		assertEquals(6, gercekSonuc);
+	}
+
 }
