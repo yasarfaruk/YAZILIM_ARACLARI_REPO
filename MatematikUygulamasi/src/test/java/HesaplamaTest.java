fb32a727794e8b072340e07d5629fa3c647df7c0
yasarfaruk <yasarfaruk-37-f@hotmail.com>
2017-12-23 14:15:07 +0300

    yfy2

--- MatematikUygulamasi/src/test/java/HesaplamaTest.java
+++ MatematikUygulamasi/src/test/java/HesaplamaTest.java
@@ -1,32 +1,16 @@
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 public class HesaplamaTest {
 
-	@Test
-	public void testToplaHepsi() {
-		
-		for (int sayi1=-10000; sayi1<10001; sayi1++) {
-			for (int sayi2=-10000;sayi2<10001; sayi2++) {
-				int gercekSonuc = Hesaplama.topla(sayi1, sayi2);
-				assertEquals(sayi1+sayi2, gercekSonuc);
-			}
-		}
+	public static void main(String[] args) {
+		int sonuc = karekok(0);
 		
 	}
 	
 	
-	@Test
-	public void testTopla25ve14() {
-		int gercekSonuc = Hesaplama.topla(25, 14);
-		assertEquals(39, gercekSonuc);
+	public static int karekok(int sayi1) {
+		    double sayi = 99;
+	        String Karekok = Double.toString(Math.sqrt(sayi));
+	        System.out.println("Karekok: " + Karekok);
+			return sayi1;
 	}
 	
-	@Test
-	public void testToplaNegatif() {
-		int gercekSonuc = Hesaplama.topla(-25, -14);
-		assertEquals(-39, gercekSonuc);
-	}
-
 }
